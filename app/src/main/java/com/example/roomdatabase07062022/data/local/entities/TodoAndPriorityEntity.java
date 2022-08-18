65c70cf8b66e8957e2f110927d65d26492e8a7c0
Phat <phatdroid94@gmail.com>
2022-08-18 20:14:22 +0700

    todoAndPriority

---
+++ app/src/main/java/com/example/roomdatabase07062022/data/local/entities/TodoAndPriorityEntity.java
@@ -0,0 +1,18 @@
+package com.example.roomdatabase07062022.data.local.entities;
+
+import androidx.room.Embedded;
+import androidx.room.Relation;
+
+/**
+ * Created by pphat on 8/18/2022.
+ */
+public class TodoAndPriorityEntity {
+    @Embedded
+    public PriorityEntity priorityEntity;
+
+    @Relation(
+            parentColumn = "id",
+            entityColumn = "id_priority"
+    )
+    public ToDoEntity toDoEntity;
+}
---
+++ app/src/main/java/com/example/roomdatabase07062022/data/model/TodoModel.java
@@ -0,0 +1,7 @@
+package com.example.roomdatabase07062022.data.model;
+
+/**
+ * Created by pphat on 8/18/2022.
+ */
+public class TodoModel {
+}
